20eb604283e5cd1b23cbd1d7ef0c54788813c4a6
Iago Mauricio <iagomauricio7@gmail.com>
2023-10-01 16:53:48 -0300

    Versão final

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
 import xmltodict # Transformar arquivos .xml em dicionários
 import os   # Manusear arquivos
 import json #Converter o arquivo em JSON para facilitar a visualização
+import pandas
 
-def pegar_infos(nome_arquivo):
+def pegar_infos(nome_arquivo, valores):
     print(f"Pegou as informações {nome_arquivo}")
     with open(f"nfs/{nome_arquivo}", "rb") as arquivo_xml: #usa-se o 'rb' para que não leia em string e sim em bytes object
-        
-        try:    
+          
             dic_arquivo = xmltodict.parse(arquivo_xml)
             print(json.dumps(dic_arquivo, indent=4))
-            infos_nf = dic_arquivo["NFe"]["infNFe"]
             
+            if "NFe" in dic_arquivo:
+                infos_nf = dic_arquivo["NFe"]["infNFe"]
+            else:
+                infos_nf = dic_arquivo["nfeProc"]["NFe"]["infNFe"]
             numero_nota = infos_nf["@Id"]
             empresa_emissora = infos_nf["emit"]['xNome']
             nome_cliente = infos_nf["dest"]["xNome"]
-            quantidade = infos_nf["dest"]["enderDest"]
-            peso_bruto = infos_nf["transp"]["vol"]["pesoB"]
+            endereco = infos_nf["dest"]["enderDest"]
+            
+            if "vol" in infos_nf["transp"]:
+                peso_bruto = infos_nf["transp"]["vol"]["pesoB"]
+            else:
+                peso_bruto = "Não informado"
 
-            print(numero_nota, empresa_emissora, nome_cliente, quantidade, peso_bruto, sep="\n")
-        except Exception as e:
-            print(e)
-            print(json.dumps(dic_arquivo, indent=4))
+            valores.append([numero_nota, empresa_emissora, nome_cliente, endereco, peso_bruto])
 
 lista_arquivos = os.listdir('nfs') # Listar os arquivos da pasta nfs
 
+colunas = ["numero_nota", "empresa_emissora", "nome_cliente", "endereco", "peso_bruto"]
+
+valores = []
+
 for arquivo in lista_arquivos:
-    pegar_infos(arquivo)
+    pegar_infos(arquivo, valores)
+
+tabela = pandas.DataFrame(columns=colunas, data=valores)
+tabela.to_excel("NotasFiscais.xlsx", index=False) 
