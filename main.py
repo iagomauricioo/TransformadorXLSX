6f6be28a4e5389723e0da9d7f640a20bd1524717
Iago Mauricio <iagomauricio7@gmail.com>
2023-10-01 10:48:16 -0300

    Versão com JSON

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import xmltodict # Transformar arquivos .xml em dicionários
 import os   # Manusear arquivos
+import json
+
 
 def pegar_infos(nome_arquivo):
     print(f"Pegou as informações {nome_arquivo}")
     with open(f"nfs/{nome_arquivo}", "rb") as arquivo_xml: #usa-se o 'rb' para que não leia em string e sim em bytes object
         dic_arquivo = xmltodict.parse(arquivo_xml)
-        print(dic_arquivo)
+        print(json.dumps(dic_arquivo, indent=4))
 
 
 lista_arquivos = os.listdir('nfs') # Listar os arquivos da pasta nfs
 
 for arquivo in lista_arquivos:
     pegar_infos(arquivo)
+    break
 
